dff9101a8500bc345a028b12df169d14837c414f
YonchRibak <109977685+YonchRibak@users.noreply.github.com>
2026-04-13 20:35:39 +0300

    stripped from app content

--- server/index.js
+++ server/index.js
@@ -1,21 +1,6 @@
-import express from 'express'
-import delay from './middleware/delay.js'
-import moviesRouter from './routes/movies.js'
-import cors from 'cors'
+import config from './config.js'
+import app from './app.js'
 
-
-const app = express()
-const PORT = 3001
-
-
-app.use(cors())
-app.use(delay)
-app.use('/movies', moviesRouter)
-
-app.get('/', (req, res) => {
-  res.json({ message: 'Server is running' })
-})
-
-app.listen(PORT, () => {
-  console.log(`Server listening on http://localhost:${PORT}`)
+app.listen(config.port, () => {
+  console.log(`Server listening on http://localhost:${config.port}`)
 })
