f431b56fd4cdb558d9472dce51edd50a5a62d515
YonchRibak <109977685+YonchRibak@users.noreply.github.com>
2026-04-13 10:15:57 +0300

    main page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,24 @@
 import { useState } from 'react'
 import { useMovies } from './hooks/useMovies'
 import { SearchBar } from './components/SearchBar'
+import { MovieCard } from './components/MovieCard'
 
 function App() {
   const [query, setQuery] = useState('')
   const { movies, loading } = useMovies(query)
 
   return (
-    <div>
-      <SearchBar query={query} onQueryChange={setQuery} />
-      <pre>{loading ? 'Loading...' : JSON.stringify(movies, null, 2)}</pre>
+    <div className="min-h-screen bg-gray-50 px-4 py-8">
+      <div className="max-w-3xl mx-auto flex flex-col gap-6">
+        <SearchBar query={query} onQueryChange={setQuery} />
+        {loading ? (
+          <p className="text-center text-gray-500">Loading...</p>
+        ) : (
+          movies.map((movie) => (
+            <MovieCard key={movie.id} movie={movie} />
+          ))
+        )}
+      </div>
     </div>
   )
 }
